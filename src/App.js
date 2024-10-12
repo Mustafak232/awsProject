06275b823b3df03cbe9b7a1f4ea8e9ce4767b1c1
Mustafa Kohistani <Kohistanim0@gmail.com>
2024-10-11 23:36:03 -0400

    Updated App.js with chimpanzee image

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // assuming your styles are in App.css
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Welcome to My Amplify App</h1>
+        <img
+          src="https://preview.redd.it/2yxc0ymalg061.jpg?width=1080&crop=smart&auto=webp&s=29985c2e747568a13c6328a6fd5cadab11c52001"
+          className="App-logo"
+          alt="Chimpanzee"
+          style={{ width: '400px', height: 'auto', borderRadius: '10px' }}
+        />
+        <p>Got You good</p>
         <a
           className="App-link"
           href="https://reactjs.org"
